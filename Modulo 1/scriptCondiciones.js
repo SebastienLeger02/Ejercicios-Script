1047e81af062773d7c88f192fd8c9a361c23fa5d
SebastienLeger02 <leger02@gmail.com>
2023-03-20 23:43:37 +0100

    commit Ejercicios 20.03

--- Modulo 1/scriptCondiciones.js
+++ Modulo 1/scriptCondiciones.js
@@ -24,7 +24,7 @@ if(sumaNumero % 2 == 0 ) {
     console.log( "La suma de los numeros ",primeroNumero, " y ", segundoNumero, " hacen ", sumaNumero, " y es impar.");
 };
 */
-
+/*
 // Ejercicio 3
 
 let numero = parseInt(prompt("Cual es tu numero?"));
@@ -41,4 +41,38 @@ if (primo === true) {
 } else  {
     console.log(numero, " no es un numero primo");
 }
+*/
+
+// Ejercicio 4
+/*
+Pedir al usario un numero
+Pedir al usario un otro numero 
+
+Hacer una condicion que permite de decir sur numero1 es mas grande que numero2
+y una otra para pedir si numero2 es mas grande que numero1.
+*/
+/*
+let numero01 = parseInt(prompt("Cual es tu primero numero?"));
+let numero02 = parseInt(prompt("Cual es tu segundo numero?"));
+
+function mayorOMenor (n1, n2) {
+    if(n1 < n2) {
+        console.log(n2, "es mayor que", n1);
+    } else if(n2 < n1) {
+        console.log(n1, " es mayor que", n2);
+    }
+}
+
+mayorOMenor(numero01,numero02);
+*/
+
+// Ejercicio 5
+/*
+Pedir al usario una letra
+
+*/
+
+
+
+
 
